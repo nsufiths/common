270441484e6fa0ed08d9ef4767a7c5f8f64a715c
evangelionexpert <pavel.smolyakov.83@gmail.com>
2021-09-28 16:56:33 +0700

    hello world

---
+++ homework1.hs
@@ -0,0 +1,14 @@
+-- Homework 1.
+-- Function qeq_roots returns a list of roots of a quadratic equation.
+
+qeq_roots :: Float -> Float -> Float -> [Float]
+qeq_roots 0 0 0 = error "x is an element of (-inf; +inf)."
+qeq_roots 0 0 c = error "no roots."
+qeq_roots 0 b c = [x] where x = -c / b
+qeq_roots a b c = roots where d = b^2 - 4*a*c 
+                              roots | d < 0 = error "discriminant is less than 0; no roots."
+                                    | d == 0 = let x = (-b) / (2*a) in [x] 
+                                    | otherwise = let x1 = (-b - sqrt d) / (2*a)
+                                                      x2 = (-b + sqrt d) / (2*a) in [x1, x2]
+
+                                                      
